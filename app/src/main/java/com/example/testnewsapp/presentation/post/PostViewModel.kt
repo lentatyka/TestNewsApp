ce831e9a5e11286a9673326252a7837a8a1932d4
lentatyka <kentatyka@gmail.com>
2022-06-16 00:30:40 +0500

    README.MD

--- app/src/main/java/com/example/testnewsapp/presentation/post/PostViewModel.kt
+++ app/src/main/java/com/example/testnewsapp/presentation/post/PostViewModel.kt
@@ -1,13 +1,12 @@
 package com.example.testnewsapp.presentation.post
 
-import android.util.Log
+
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.testnewsapp.common.State
 import com.example.testnewsapp.data.post.Post
 import com.example.testnewsapp.domain.post.GetPostUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.scopes.FragmentScoped
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
--- app/src/main/java/com/example/testnewsapp/presentation/post/PostsFragment.kt
+++ app/src/main/java/com/example/testnewsapp/presentation/post/PostsFragment.kt
@@ -1,19 +1,16 @@
 package com.example.testnewsapp.presentation.post
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.testnewsapp.R
 import com.example.testnewsapp.common.State
 import com.example.testnewsapp.databinding.FragmentPostsBinding
 import dagger.hilt.android.AndroidEntryPoint
