480fef7ee42a6ec3b28a71dbc0e950f1fc072dc9
lentatyka <kentatyka@gmail.com>
2022-06-15 18:23:24 +0500

    Initial commit

---
+++ app/src/main/java/com/example/testnewsapp/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.testnewsapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.testnewsapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
